07e4852b138843737bcbc370ce85dd8e617cc3a3
nuyhc <spec327@naver.com>
2022-10-09 21:09:21 +0900

    파이썬 기초 문법 후반부 리딩 자료

---
+++ 01_Python_Basic/calc.py
@@ -0,0 +1,13 @@
+class Calculator:
+    @staticmethod
+    def add(n1, n2):
+        return n1 + n2
+    @staticmethod
+    def sub(n1, n2):
+        return n1 - n2
+    @staticmethod
+    def mul(n1, n2):
+        return n1 * n2
+    @staticmethod
+    def div(n1, n2):
+        return n1/n2
